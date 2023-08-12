2c273296ee910b218251ea8c69cf55ef4964c8a9
DESKTOP-6232LEE\user <steventorresf@gmail.com>
2023-08-12 12:32:04 -0500

    Se instala react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { Form, FormGroup, FormLabel } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      
+      <Form>
+        <FormGroup>
+          <FormLabel>Nombre de usuario:</FormLabel>
+          <input className='form-control' />
+        </FormGroup>
+      </Form>
     </div>
   );
 }
