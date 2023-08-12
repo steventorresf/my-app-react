b33e25136e3d2e2e04260a8430ad56555eaf5c5b
DESKTOP-6232LEE\user <steventorresf@gmail.com>
2023-08-12 12:36:31 -0500

    Se hace formulario de inicio de sesion

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ function App() {
       <Form>
         <FormGroup>
           <FormLabel>Nombre de usuario:</FormLabel>
-          <input className='form-control' />
+          <input className='form-control' required />
+        </FormGroup>
+        <FormGroup className='mt-3'>
+          <FormLabel>Contraseña:</FormLabel>
+          <input type='password' className='form-control' required />
         </FormGroup>
       </Form>
     </div>
