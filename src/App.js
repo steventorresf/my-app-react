e0f32aaacf5a2da7ffd1d02e25a976eed81430a9
DESKTOP-6232LEE\user <steventorresf@gmail.com>
2023-08-12 13:08:32 -0500

    Se instala formik

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 import { Button, Form, FormGroup, FormLabel } from "react-bootstrap";
+import Swal from 'sweetalert2';
+import * as Yup from 'yup';
+
+const yupSchema = Yup.object().shape({
+    tipoId: Yup.object().required("Este campo es obligatorio"),
+    identificacion: Yup.string().required("Este campo es obligatorio"),
+    nombreCompleto: Yup.string().required("Este campo es obligatorio"),
+    generoId: Yup.object().required("Este campo es obligatorio"),
+    direccion: Yup.string().required("Este campo es obligatorio"),
+    telCel: Yup.string().required("Este campo es obligatorio")
+})
 
 function App() {
   function onSubmit() {
-
+    Swal.fire({
+      title: '¡Proceso exitoso!',
+      text: 'Inicio de sesión correcto'
+    })
   }
-  
+
   return (
     <div className="App">
-      <Form onSubmit={onSubmit}>
+      <Form>
         <FormGroup>
           <FormLabel>Nombre de usuario:</FormLabel>
           <input className='form-control' required />
@@ -19,7 +32,7 @@ function App() {
           <input type='password' className='form-control' required />
         </FormGroup>
         <FormGroup className='mt-3 text-center'>
-          <Button type='submit'>Iniciar sesión</Button>
+          <Button type='button' onClick={onSubmit}>Iniciar sesión</Button>
         </FormGroup>
       </Form>
     </div>
